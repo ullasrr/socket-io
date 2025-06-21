22a5bf1cc9d83f38fa1cbe77d2a3902e1cb9b491
Ullas R <ullasr2004@gmail.com>
2025-06-21 23:15:45 +0530

    c

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,80 @@
-
+import React, { useEffect,useState,useRef}  from 'react'
+import {io} from 'socket.io-client'
 import './App.css'
+import {Button, Container, TextField, Typography,Box, Stack} from '@mui/material'
 
 function App() {
+  
+  const [message, setmessage] = useState("");
+  const [room, setroom] = useState("");
+  const socket = useRef(null);
+  const [socketid, setSocketId] = useState("");
+  const [messages, setmessages] = useState([]);
+  
+  console.log(messages);
+  useEffect(() => {
+    socket.current=io('http://localhost:3000');
+
+    socket.current.on("connect",()=> {
+      console.log("Connected to server with ID:", socket.current.id)
+      setSocketId(socket.current.id);
+    });
+    socket.current.on("message",(data)=>{
+      console.log("Message received:", data);
+      // You can update the UI or state here to display the message
+      setmessages((messages)=> [...messages,data]);
+    })
+
+    // socket.current.on("welcome", (message) => {
+    //   console.log(message)
+    // });
+    return()=>{
+      socket.current.disconnect();
+    };
+  },[])
+
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    if(socket.current){
+      socket.current.emit("message", {message,room});
+      setmessage("");
+      setroom("");
+    }
+    
+  }
 
   return (
     <>
-      
+      <div>
+        <Container maxWidth="sm" className="App">
+          <Box sx={{  padding: 2 }}>
+
+          <Typography>
+          Socket.IO Client
+          </Typography>
+
+          <Typography variant='h6' component='div' gutterBottom>
+            This is socket id : {socket.current?.id ?? "connecting..."}
+          </Typography>
+
+          <form action="" onSubmit={handleSubmit}>
+            <TextField label="Message" fullWidth margin="normal" value={message} onChange={(e) => setmessage(e.target.value)} />
+              <TextField label="Room" fullWidth margin="normal" value={room} onChange={(e) => setroom(e.target.value)} />
+            <Button type='submit' variant="contained" color="primary">Send</Button>
+
+          </form>
+<Stack>
+{messages.map((msg, i) => (
+  <Typography key={i}>
+    {typeof msg === "string" ? msg : `${msg.message} `}
+  </Typography>
+))}
+</Stack>
+
+
+          </Box>
+        </Container>
+      </div>
     </>
   )
 }
--- server/app.js
+++ server/app.js
@@ -1,13 +1,26 @@
 import express from "express";
 import {Server} from 'socket.io';
 import { createServer } from "http";
+import cors from "cors";
 
 const port=3000;
 const app=express();
 const httpServer= createServer(app);
 
 
-const io = new Server(httpServer);
+const io = new Server(httpServer,{
+    cors:{
+        origin:"http://localhost:5173",
+        methods:["GET","POST"],
+        credentials:true,
+    }
+});
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+}));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -16,6 +29,14 @@ app.get("/", (req, res) => {
 io.on("connection",(socket)=>{
     console.log("user connected");
     console.log("ID",socket.id);
+    
+    socket.on("message",(data)=>{
+        console.log("message received",data);
+        // console.log("room number",room);
+        // Emit the message to all connected clients
+        io.to(data.room).emit("message",data);
+        // send to all including sender
+    })
 })
 
 
